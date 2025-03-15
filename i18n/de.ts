6509eaba13c0ecf7e817e43f8fd3c6fd704126df
Tim Schneider <t.schneider@dahlem-ingenieure.de>
2025-03-15 18:37:19 +0100

    add German translation .ts file

---
+++ i18n/de.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>SplashToolResultLoader</name>
+    <message>
+        <location filename="splashtool_result_loader.py" line="22"/>
+        <source>Load Latest Files</source>
+        <translation>Neueste Dateien laden</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="54"/>
+        <source>Load results from SplashTool</source>
+        <translation>Ergebnisse von SplashTool laden</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="70"/>
+        <source>Custom Plugins</source>
+        <translation>Benutzerdefinierte Plugins</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="67"/>
+        <source>&amp;SplashTool Result Loader</source>
+        <translation>&amp;SplashTool Ergebnis-Lader</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="73"/>
+        <source>Select Input Folder</source>
+        <translation>Eingabeordner auswählen</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="104"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="104"/>
+        <source>Failed to load {}</source>
+        <translation>Fehler beim Laden von {}</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="116"/>
+        <source>Warning</source>
+        <translation>Warnung</translation>
+    </message>
+    <message>
+        <location filename="splashtool_result_loader.py" line="116"/>
+        <source>QML file not found for {}: {}</source>
+        <translation>QML-Datei nicht gefunden für {}: {}</translation>
+    </message>
+</context>
+</TS>
